7c7d4b1ab9267f0f021c12a72d8d737524d60c82
Meghun96 <mimwillemsen@gmail.com>
2017-02-15 14:39:25 +0100

    Simple agent class

---
+++ src/pursuitevasion/Agent.java
@@ -0,0 +1,87 @@
+package pursuitevasion;
+
+/**
+ * Created by Megan on 15/02/2017.
+ */
+public class agent {
+
+    boolean pursuer,moveable,communicating;
+    int level,algo;
+    double speed,FOV,range;
+
+
+
+
+
+    public agent(boolean hunter, int type){
+        this.pursuer= hunter;
+        this.level=type;
+
+
+
+        if(type==1){
+            SetSpeed(type);
+            SetFOV(180);
+            SetAlgo(1);
+            //this.algo= preset1;
+            //this.moveable=preset1;
+            //this.communicating=preset1;
+        }
+        else if(type==2){
+            SetSpeed(type);
+            SetFOV(360);
+            SetAlgo(2);
+        }
+        //etc
+
+    }
+
+    public agent(boolean hunter, int type,int algorithm, boolean moveable, boolean communicating, double range){
+        this.pursuer= hunter;
+        this.level=type;
+        this.algo= algorithm;
+        this.moveable=moveable;
+        this.communicating=communicating;
+        this.range=range;
+
+
+        if(type==1){
+            SetSpeed(type);
+            SetFOV(180);
+            SetAlgo(1);
+        }
+        else if(type==2){
+            SetSpeed(type);
+            SetFOV(360);
+            SetAlgo(2);
+        }
+        //etc
+
+    }
+
+    public void SetSpeed(int type){
+        if(this.moveable)this.speed=type*1000;
+        else this.speed=0;
+    }
+    public double getSpeed(agent Agent){
+        return Agent.speed;
+    }
+
+    public void SetFOV(double fov){
+        this.FOV=fov;
+    }
+
+    public double getFOV(agent Agent){
+        return Agent.FOV;
+    }
+
+    public void SetAlgo(int algo){
+        this.algo=algo;
+    }
+
+    public double getalgo(agent Agent){
+        return Agent.algo;
+    }
+
+
+}
