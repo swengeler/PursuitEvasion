8bea78f7377bc55f99d03d82e16ff90e422279d0
jyrr <jordy.qwerty@gmail.com>
2017-05-26 22:04:56 +0200

    hiding evader wip

--- src/simulation/HideEvaderPolicy.java
+++ src/simulation/HideEvaderPolicy.java
@@ -5,13 +5,24 @@ import javafx.collections.ObservableList;
 import javafx.geometry.Point2D;
 import javafx.scene.shape.Line;
 import javafx.scene.shape.Polygon;
-import javafx.util.Pair;
 import org.javatuples.Triplet;
+import org.jdelaunay.delaunay.ConstrainedMesh;
+import org.jdelaunay.delaunay.error.DelaunayError;
+import org.jdelaunay.delaunay.geometries.DEdge;
+import org.jdelaunay.delaunay.geometries.DPoint;
+import org.jdelaunay.delaunay.geometries.DTriangle;
 import pathfinding.ShortestPathRoadMap;
 
 import java.util.ArrayList;
+import java.util.List;
 
 public class HideEvaderPolicy extends MovePolicy {
+
+    private TraversalHandler traversalHandler;
+    private PlannedPath currentPath;
+
+    ArrayList<Line> pathLines;
+    int i = 0;
 
     public HideEvaderPolicy(Agent agent, boolean pursuing, MapRepresentation map) {
         super(agent, pursuing);
@@ -19,21 +30,25 @@ public class HideEvaderPolicy extends MovePolicy {
 
     @Override
     public Move getNextMove(MapRepresentation map, ArrayList<Agent> agents) {
-        ArrayList<Point2D> polygonMidpoints = getPolygonMidpoints(map);
-        ShortestPathRoadMap shortestPathMap = new ShortestPathRoadMap(map);
+        //boolean pathChanged = false;
 
-        System.out.println(polygonMidpoints.size());
+        if (traversalHandler == null) {
+            initTree(map);
+        }
+
+        ArrayList<Point2D> polygonMidpoints = getPossiblePolygonPoints(map);
+        ShortestPathRoadMap shortestPathMap = new ShortestPathRoadMap(map);
 
         Agent evader = getSingleAgent();
         ArrayList<Triplet<Point2D, Double, Integer>> midpointData = new ArrayList<>();
         Point2D target = null;
 
-        for (Point2D midpoint: polygonMidpoints) {
+        for (Point2D midpoint : polygonMidpoints) {
 
             double midpointDistance = 0;
             int numberOfVertices = 0;
 
-            for (Agent pursuer: agents) {
+            for (Agent pursuer : agents) {
 
                 if (pursuer.isPursuer()) {
 
@@ -51,26 +66,39 @@ public class HideEvaderPolicy extends MovePolicy {
 
         }
 
-        target = getMin(midpointData, 1);
+        //mode 0 = eucl dist
+        //mode 1 = num of verts
+
+        //idea:
+        //num of verts is more important, but if equal eucl distance determines
+
+
+        target = getMin(midpointData, 0);
         double dx = 0;
         double dy = 0;
 
         if (target != null) {
-            PlannedPath pathToTarget = shortestPathMap.getShortestPath(new Point2D(getSingleAgent().getXPos(), getSingleAgent().getYPos()), target);
-            dx = pathToTarget.getPathLines().get(0).getEndX();
-            dy = pathToTarget.getPathLines().get(0).getEndY();
-
-            System.out.println("SHOULD MOVE TO " + dx + ":" + dy);
+            currentPath = shortestPathMap.getShortestPath(new Point2D(getSingleAgent().getXPos(), getSingleAgent().getYPos()), target);
         }
 
-        if (map.legalPosition(dx * evader.getSpeed() * 1 / 250, dy * evader.getSpeed() * 1 / 250)) {
-            return new Move(dx * evader.getSpeed() * 1 / 250, dy * evader.getSpeed() * 1 / 250, 0);
-        }  else {
-            System.out.println("MOVE OUT OF BOUNDS: STAND STILL");
-            return new Move(0, 0, 0);
+        pathLines = currentPath.getPathLines();
+
+        Move result;
+        double length = Math.sqrt(Math.pow(pathLines.get(i).getEndX() - pathLines.get(i).getStartX(), 2) + Math.pow(pathLines.get(i).getEndY() - pathLines.get(i).getStartY(), 2));
+        double deltaX = (pathLines.get(i).getEndX() - pathLines.get(i).getStartX()) / length * getSingleAgent().getSpeed() / 50;
+        double deltaY = (pathLines.get(i).getEndY() - pathLines.get(i).getStartY()) / length * getSingleAgent().getSpeed() / 50;
+
+        if (pathLines.get(i).contains(evader.getXPos() + deltaX, evader.getYPos() + deltaY)) {
+            // move along line
+            result = new Move(deltaX, deltaY, 0);
+        } else {
+            result = new Move(pathLines.get(i).getEndX() - getSingleAgent().getXPos(), pathLines.get(i).getEndY() - getSingleAgent().getYPos(), 0);
+            i++;
         }
 
+        return result;
     }
+
 
     private Point2D getMin(ArrayList<Triplet<Point2D, Double, Integer>> midpointData, int mode) {
         Point2D target = null;
@@ -100,13 +128,19 @@ public class HideEvaderPolicy extends MovePolicy {
         return target;
     }
 
-    private ArrayList<Point2D> getPolygonMidpoints(MapRepresentation map) {
-        ArrayList<Polygon> polygons = map.getAllPolygons();
-        ArrayList<Point2D> midpoints = new ArrayList<>();
+    private ArrayList<Point2D> getPossiblePolygonPoints(MapRepresentation map) {
+        ArrayList<Point2D> polygonPoints = new ArrayList<>();
 
-        for (Polygon p : polygons) {
-            ObservableList<Double> singlePoints = p.getPoints();
-            ArrayList<Point2D> points = new ArrayList<>();
+        ObservableList<Double> singlePoints = map.getBorderPolygon().getPoints();
+        ArrayList<Point2D> points = new ArrayList<>();
+
+        for (int i = 0; i < singlePoints.size(); i += 2) {
+            polygonPoints.add(new Point2D(singlePoints.get(i), singlePoints.get(i + 1)));
+        }
+
+        for (Polygon p : map.getObstaclePolygons()) {
+            singlePoints = p.getPoints();
+            points.clear();
 
             for (int i = 0; i < singlePoints.size(); i += 2) {
                 points.add(new Point2D(singlePoints.get(i), singlePoints.get(i + 1)));
@@ -123,11 +157,52 @@ public class HideEvaderPolicy extends MovePolicy {
                 double x = (pointOne.getX() + pointTwo.getX()) / 2;
                 double y = (pointOne.getY() + pointTwo.getY()) / 2;
 
-                midpoints.add(new Point2D(x, y));
+                polygonPoints.add(new Point2D(x, y));
             }
         }
 
-        return midpoints;
+        return polygonPoints;
+    }
+
+    private void initTree(MapRepresentation map) {
+        try {
+            ArrayList<DEdge> constraintEdges = new ArrayList<>();
+            ArrayList<Line> polygonEdges = map.getPolygonEdges();
+            ArrayList<Polygon> polygons = map.getAllPolygons();
+            for (Line l : polygonEdges) {
+                constraintEdges.add(new DEdge(new DPoint(l.getStartX(), l.getStartY(), 0), new DPoint(l.getEndX(), l.getEndY(), 0)));
+            }
+
+            ConstrainedMesh mesh = new ConstrainedMesh();
+            mesh.setConstraintEdges(constraintEdges);
+            mesh.processDelaunay();
+            List<DTriangle> triangles = mesh.getTriangleList();
+            List<DTriangle> includedTriangles = new ArrayList<>();
+
+            for (DTriangle dt : triangles) {
+                // check if triangle in polygon
+                double centerX = dt.getBarycenter().getX();
+                double centerY = dt.getBarycenter().getY();
+                boolean inPolygon = true;
+                if (!polygons.get(0).contains(centerX, centerY)) {
+                    inPolygon = false;
+                }
+                for (int i = 1; inPolygon && i < polygons.size() - 1; i++) {
+                    if (polygons.get(i).contains(centerX, centerY)) {
+                        inPolygon = false;
+                    }
+                }
+                if (inPolygon) {
+                    includedTriangles.add(dt);
+                }
+            }
+
+            traversalHandler = new TraversalHandler((ArrayList<DTriangle>) includedTriangles);
+            traversalHandler.shortestPathRoadMap = new ShortestPathRoadMap(map);
+            traversalHandler.map = map;
+        } catch (DelaunayError e) {
+            e.printStackTrace();
+        }
     }
 
 }
